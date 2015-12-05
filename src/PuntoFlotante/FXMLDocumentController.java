17839b64ed504384c9cb1b0ab654fbe21d118712
Javier Flores <javierfh1404@gmail.com>
2015-12-05 16:54:23 -0600

    Correcciones

---
+++ src/PuntoFlotante/FXMLDocumentController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PuntoFlotante;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Javier
+ */
+public class FXMLDocumentController implements Initializable {
+    // c:
+    private void openWindowWithOption(String file) {
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+
+            root = FXMLLoader.load(getClass().getResource(file));
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.show();
+    }
+    
+    @FXML private void Biseccion(){
+        openWindowWithOption("/Biseccion/FXMLBiseccion.fxml");
+    }
+    
+    @FXML private void FalsePosition(){
+       openWindowWithOption("/FalsePosition/FXMLFalsePosition.fxml");
+    }
+    
+    @FXML private void NewtonR(){
+       
+    }
+    
+    @FXML private void PuntoFijo(){
+       
+    }
+    
+    @FXML private void Secante(){
+       
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/PuntoFlotante/Truncamiento.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PuntoFlotante;
+
+/**
+ *
+ * @author LosPros
+ */
+public class Truncamiento {
+// c:
+    /**
+     * Función para truncar un número decimal
+     *
+     * @param double nD, número que se quiere truncar
+     * @param int nDec, cantidad de decimales
+     * @return double que representa el nuevo número truncado
+     */
+    public double getTruncamiento(double nD, int nDec) {
+        if (nD > 0) {
+            nD = Math.floor(nD * Math.pow(10, nDec)) / Math.pow(10, nDec);
+        } else {
+            nD = Math.ceil(nD * Math.pow(10, nDec)) / Math.pow(10, nDec);
+        }
+        return nD;
+    }
+}
