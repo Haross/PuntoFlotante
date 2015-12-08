79f5d3a28b39d3d02c64926ff09d6e63c1d18bf5
Javier Flores <javierfh1404@gmail.com>
2015-12-08 09:24:03 -0600

    Lagrange por puntos

--- src/Lagrange/FXMLLagrangeController.java
+++ src/Lagrange/FXMLLagrangeController.java
@@ -35,31 +35,131 @@ public class FXMLLagrangeController implements Initializable {
     @FXML
     public void calculo() {
         txtArea.setText("");
-        getP3();
+        switch(num){
+            case "2":
+                getP1();
+                break;
+            case "3":
+                getP2();
+                break;
+            case "4":
+                getP3();
+                break;
+        }
+        
     }
 
     public void getXY() {
+        int i = Integer.parseInt(numNodos.getValue().toString());
+        for (int j = 1; j <= i; j++) {
+            try {
+                switch (j) {
+                    case 1:
+                        x0 = new BigDecimal(txtx0.getText());
+                        y0 = new BigDecimal(txty0.getText());
+                        break;
+                    case 2:
+                        x1 = new BigDecimal(txtx1.getText());
+                        y1 = new BigDecimal(txty1.getText());
+                        break;
+                    case 3:
+                        x2 = new BigDecimal(txtx2.getText());
+                        y2 = new BigDecimal(txty2.getText());
+                        break;
+                    case 4:
 
-        x0 = new BigDecimal(txtx0.getText());
-        y0 = new BigDecimal(txty0.getText());
-        x1 = new BigDecimal(txtx1.getText());
-        y1 = new BigDecimal(txty1.getText());
-        x2 = new BigDecimal(txtx2.getText());
-        y2 = new BigDecimal(txty2.getText());
-        x3 = new BigDecimal(txtx3.getText());
-        y3 = new BigDecimal(txty3.getText());
+                        x3 = new BigDecimal(txtx3.getText());
+                        y3 = new BigDecimal(txty3.getText());
+                        break;
+                }
+            } catch (Exception e) {
+
+            }
+        }
 
     }
-    public BigDecimal getDivision(BigDecimal a, BigDecimal b){
-         MathContext mc = new MathContext(10, RoundingMode.DOWN);
-              /*  if(tipo == 2){
-                     mc = new MathContext(k,  RoundingMode.HALF_EVEN);
+
+    public BigDecimal getDivision(BigDecimal a, BigDecimal b) {
+        MathContext mc = new MathContext(10, RoundingMode.DOWN);
+        /*  if(tipo == 2){
+         mc = new MathContext(k,  RoundingMode.HALF_EVEN);
                    
-                }*/
-              return a.divide(b, mc);
-               
+         }*/
+        return a.divide(b, mc);
+
     }
     
+    public void getP1(){
+        getXY();
+        BigDecimal a = x0.subtract(x1);
+        BigDecimal b = x1.subtract(x0);
+        BigDecimal a1 = getDivision(new BigDecimal(1), a);
+        BigDecimal b1 = getDivision(new BigDecimal(1), b);
+        txtArea.setText("L0 = (x - " + x1 + ")* " + a1);
+        txtArea.setText(txtArea.getText() + "\n\nL1 = (x - " + x0 + ")  *" + b1);
+         a = getDivision(y0, a);
+        b = getDivision(y1, b);
+        
+        BigDecimal c1 = a.add(b);
+        BigDecimal c2 =  a.multiply(x1).add(b.multiply(x0)).multiply(new BigDecimal(-1));
+        if (isNegative(c1)) {
+            txtArea.setText(txtArea.getText() + "\n\n" + c1 + "x  ");
+        } else {
+            txtArea.setText(txtArea.getText() + "\n\n" + c1 + "x  ");
+        }
+        if (isNegative(c2)) {
+            txtArea.setText(txtArea.getText() + c2 );
+        } else {
+            txtArea.setText(txtArea.getText() +"+"+ c2 );
+        }
+        
+    }
+
+    public void getP2() {
+        getXY();
+        BigDecimal a = x0.subtract(x1).multiply(x0.subtract(x2));
+
+        BigDecimal b = x1.subtract(x0).multiply(x1.subtract(x2));
+
+        BigDecimal c = x2.subtract(x0).multiply(x2.subtract(x1));
+
+        BigDecimal a1 = getDivision(new BigDecimal(1), a);
+        BigDecimal b1 = getDivision(new BigDecimal(1), b);
+        BigDecimal c1 = getDivision(new BigDecimal(1), c);
+        txtArea.setText("L0 = (x - " + x1 + ") * (x - " + x2 + ") *" + a1);
+        txtArea.setText(txtArea.getText() + "\n\nL1 = (x - " + x0 + ") * (x - " + x2 + ") *" + b1);
+        txtArea.setText(txtArea.getText() + "\n\nL2 = (x - " + x0 + ") * (x - " + x1 + ")  *" + c1);
+
+        a = getDivision(y0, a);
+        b = getDivision(y1, b);
+        c = getDivision(y2, c);
+
+        c1 = a.add(b).add(c);
+        BigDecimal c2 = a.multiply(x1.add(x2)).add(b.multiply(x0.add(x2))).add(c.multiply(x0.add(x1))).multiply(new BigDecimal(-1));
+        BigDecimal c3 = a.multiply(x1).multiply(x2).add(b.multiply(x0).multiply(x2)).add(c.multiply(x0).multiply(x1));
+
+        if (isNegative(c1)) {
+            txtArea.setText(txtArea.getText() + "\n\n" + c1 + "x^2  ");
+        } else {
+            txtArea.setText(txtArea.getText() + "\n\n" + c1 + "x^2  ");
+        }
+        if (isNegative(c2)) {
+            txtArea.setText(txtArea.getText() + c2 + "x ");
+        } else {
+            txtArea.setText(txtArea.getText() +"+"+ c2 + "x  ");
+        }
+        if (isNegative(c3)) {
+            txtArea.setText(txtArea.getText() + c3);
+        } else {
+            txtArea.setText(txtArea.getText() +"+"+ c3);
+        }
+
+    }
+    public static boolean isNegative(BigDecimal b)
+    {
+        return b.signum() == -1;
+    }
+
     public void getP3() {
 
         getXY();
@@ -67,7 +167,7 @@ public class FXMLLagrangeController implements Initializable {
         BigDecimal b = x1.subtract(x0).multiply(x1.subtract(x2)).multiply(x1.subtract(x3));
         BigDecimal c = x2.subtract(x0).multiply(x2.subtract(x1)).multiply(x2.subtract(x3));
         BigDecimal d = x3.subtract(x0).multiply(x3.subtract(x1)).multiply(x3.subtract(x2));
- 
+
         BigDecimal a1 = getDivision(new BigDecimal(1), a);
         BigDecimal b1 = getDivision(new BigDecimal(1), b);
         BigDecimal c1 = getDivision(new BigDecimal(1), c);
@@ -75,24 +175,14 @@ public class FXMLLagrangeController implements Initializable {
         txtArea.setText("L0 = (x - " + x1 + ") * (x - " + x2 + ") * (x - +" + x3 + ") *" + a1);
         txtArea.setText(txtArea.getText() + "\n\nL1 = (x - " + x0 + ") * (x - " + x2 + ") * (x - +" + x3 + ") *" + b1);
         txtArea.setText(txtArea.getText() + "\n\nL2 = (x - " + x0 + ") * (x - " + x1 + ") * (x - +" + x3 + ") *" + c1);
-        
+
         txtArea.setText(txtArea.getText() + "\n\nL3 = (x - " + x0 + ") * (x - " + x1 + ") * (x - +" + x2 + ") *" + d1);
-a = getDivision(y0, a);
+        a = getDivision(y0, a);
         b = getDivision(y1, b);
         c = getDivision(y2, c);
         d = getDivision(y3, d);
-       // double a = ((x0 - x1) * (x0 - x2) * (x0 - x3));
-        // double b = ((x1 - x0) * (x1 - x2) * (x1 - x3));
-        // double c = ((x2 - x0) * (x2 - x1) * (x2 - x3));
-        //double d = ((x3 - x0) * (x3 - x1) * (x3 - x2));
-        
-       
-       /* a = y0 / a;
-        b = y1 / b;
-        c = y2 / c;
-        d = y3 / d;*/
-        
-       c1 = a.add(b).add(c).add(d);
+
+        c1 = a.add(b).add(c).add(d);
         BigDecimal c2 = a.multiply(x1.add(x2).add(x3)).add(b.multiply(x0.add(x2).add(x3)))
                 .add(c.multiply(x0.add(x1).add(x3))).add(d.multiply(x0.add(x1).add(x2))).abs().multiply(new BigDecimal(-1));
         BigDecimal c3 = a.multiply(x1.multiply(x2).add(x1.multiply(x3)).add(x2.multiply(x3)))
@@ -102,50 +192,34 @@ a = getDivision(y0, a);
         BigDecimal c4 = a.multiply(x1).multiply(x2).multiply(x3)
                 .add(b.multiply(x0).multiply(x2).multiply(x3))
                 .add(c.multiply(x0).multiply(x1).multiply(x3))
-                .add(d.multiply(x0).multiply(x1).multiply(x2));
-        
-        System.out.println("1.  "+a.multiply(x1).multiply(x2).multiply(x3));
-        System.out.println( a);
+                .add(d.multiply(x0).multiply(x1).multiply(x2)).multiply(new BigDecimal(-1));
+
+        System.out.println("1.  " + a.multiply(x1).multiply(x2).multiply(x3));
+        System.out.println(a);
         System.out.println(a.multiply(x1));
         System.out.println(a.multiply(x1).multiply(x2));
         System.out.println(a.multiply(x1).multiply(x2).multiply(x3));
-        
-        
-        
-        /*double c1 = a + b + c + d;
-        double c2 = -(a * (x1 + x2 + x3)
-                + b * (x0 + x2 + x3)
-                + c * (x0 + x1 + x3)
-                + d * (x0 + x1 + x2));
-        double c3 = a * (x1 * x2 + x1 * x3 + x2 * x3)
-                + b * (x0 * x2 + x0 * x3 + x2 * x3)
-                + c * (x0 * x1 + x0 * x3 + x1 * x3)
-                + d * (x0 * x1 + x0 * x2 + x1 * x2);
-        double c4 = -(a * x1 * x2 * x3
-                + b * x0 * x2 * x3
-                + c * x0 * x1 * x3
-                + d * x0 * x1 * x2);*/
-        if(c1.doubleValue() < 0){
-            txtArea.setText(txtArea.getText()+"\n\n"+c1 + "x^3  ");
-        }else{
-            txtArea.setText(txtArea.getText()+"\n\n"+c1 + "x^3  + ");
+
+        if (isNegative(c1)) {
+            txtArea.setText(txtArea.getText() + "\n\n" + c1 + "x^3  ");
+        } else {
+            txtArea.setText(txtArea.getText() + "\n\n" + c1 + "x^3   ");
         }
-        if(c2.doubleValue() < 0){
-            txtArea.setText(txtArea.getText()+c2 + "x^2  ");
-        }else{
-            txtArea.setText(txtArea.getText()+c2 + "x^2  + ");
+        if (isNegative(c2)) {
+            txtArea.setText(txtArea.getText() + c2 + "x^2  ");
+        } else {
+            txtArea.setText(txtArea.getText() + c2 +"+"+ "x^2  ");
         }
-        if(c3.doubleValue() < 0){
-            txtArea.setText(txtArea.getText()+c3 + "x  ");
-        }else{
-            txtArea.setText(txtArea.getText()+c3 + "x  + ");
+        if (isNegative(c3)) {
+            txtArea.setText(txtArea.getText() + c3 + "x  ");
+        } else {
+            txtArea.setText(txtArea.getText() + c3 +"+"+ "x  ");
         }
-         if(c4.doubleValue() < 0){
-            txtArea.setText(txtArea.getText()+c4);
-        }else{
-            txtArea.setText(txtArea.getText()+c4);
+        if (isNegative(c4)) {
+            txtArea.setText(txtArea.getText() + c4);
+        } else {
+            txtArea.setText(txtArea.getText() +"+"+ c4);
         }
-      
 
     }
 
