1572a08610d68ba1c5e1c47fdea7fa259a2c126e
JosselineA <j_uliane20@hotmail.com>
2015-12-08 10:39:08 -0600

    lagrange combobox

--- src/Lagrange/FXMLLagrangeController.java
+++ src/Lagrange/FXMLLagrangeController.java
@@ -9,6 +9,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -21,7 +22,7 @@ import javafx.scene.control.TextField;
 public class FXMLLagrangeController implements Initializable {
 
     @FXML
-    private ChoiceBox numNodos;
+    private ComboBox numNodos;
     @FXML
     private Label lblx0, lbly0, lblx1, lbly1, lblx2, lbly2, lblx3, lbly3;
     @FXML
